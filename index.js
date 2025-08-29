c0e85a12bb2980997ede2aaaec9a7bd6cfe7841a
saifur-sagor <mdsaifurrahmansagor606@gmail.com>
2025-08-29 21:22:06 +0600

    heart and coin done

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const callBtns = document.getElementsByClassName("call-btn");
 for (const callBtn of callBtns) {
   callBtn.addEventListener("click", function () {
-    alert("calling");
-    console.log("clicked");
-
     const title = callBtn.parentNode.parentNode.children[2].innerText;
     const number = callBtn.parentNode.parentNode.children[3].innerText;
 
+    // coin function
+
+    const coinSpan = document.getElementById("coin");
+    let coinValue = parseInt(coinSpan.innerText);
+
+    // coin check
+    if (coinValue < 20) {
+      alert("Coins no more left!");
+      return;
+    }
+
+    // coin minus
+    coinValue -= 20;
+    coinSpan.innerText = coinValue;
+
+    // Alert
+    alert(`Calling ${title} ${number}`);
     // cantainer call
     const callHistory = document.getElementById("history-container");
 
     const newHistory = document.createElement("div");
     newHistory.innerHTML = ` <div class="call-history">
               <div class="bg-gray-100 rounded-[6px] p-2 mt-2 mb-2">
-                <h1>${title}</h1>
+                <h1 class="font-bold">${title}</h1>
                 <p>${number}</p>
               </div>
             </div>`;
@@ -33,8 +47,10 @@ const headerHeart = document.getElementById("count");
 const cardHearts = document.getElementsByClassName("icon");
 for (const heart of cardHearts) {
   heart.addEventListener("click", function () {
-    let count = headerHeart.innerText;
+    let count = parseInt(headerHeart.innerText);
     count++;
     headerHeart.innerText = count;
+
+    // coin minus function
   });
 }
